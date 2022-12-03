19089b67155b6568940fd63d30c25cc46105a4f6
MikiastesfayeJ <Mikiastesfaye998@gmail.com>
2022-12-03 23:35:29 +0300

    my initial commit

---
+++ practice.py
@@ -0,0 +1,27 @@
+def equate(eq):
+    global op1, op, op2, sign, result
+    op1, op, op2, sign, result = eq.split()
+    if op1.lower() == 'x':
+        op2, result = int(op2), int(result)
+        if op == '+':
+            print(result - op2)
+        elif op == '-':
+            print(result + op2)
+        elif op == '*':
+            print(result / op2)
+        elif op == '/':
+            print(result * op2)
+    elif op2.lower() == 'x':
+        op1, result = int(op1), int(result)
+        if op == '+':
+            print(result - op1)
+        elif op == '-':
+            print(op1 - result)
+        elif op == '*':
+            print(result / op1)
+        elif op == '/':
+            print(result * op1)     
+
+
+a = 3
+print(a = 5)  
